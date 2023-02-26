e15018e7c22f7b83ea50d781ae4de8171d4057a3
levisstrauss <levisstrauss11@yahoo.com>
2023-02-26 18:23:56 -0500

    Self android app

---
+++ app/src/main/java/com/levisstrauss/self/PostJournalActivity.java
@@ -0,0 +1,15 @@
+package com.levisstrauss.self;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostJournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_journal);
+
+    }
+}
